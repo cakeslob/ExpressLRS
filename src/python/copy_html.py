083cd035f1493ecb9269687059e1c1d156ed460d
Frank Zhao <frank.zhao.main@gmail.com>
2026-04-15 12:04:36 -0700

    added a warning if new web header files are not built

--- src/python/copy_html.py
+++ src/python/copy_html.py
@@ -8,4 +8,9 @@ type = 'tx' if '_TX_' in target_name else 'rx'
 is8285 = '-8285' if 'ESP8285' in env['PIOENV'] else ''
 chip = 'sx128x' if '-DRADIO_SX128X=1' in env['BUILD_FLAGS'] else 'sx127x' if '-DRADIO_SX127X=1' in env['BUILD_FLAGS'] else 'lr1121' if '-DRADIO_LR1121=1' in env['BUILD_FLAGS'] else ''
 
-shutil.copy(f'headers/web-{chip}-{type}{is8285}.h', '../include/WebContent.h')
+try:
+    shutil.copy(f'headers/web-{chip}-{type}{is8285}.h', '../include/WebContent.h')
+except FileNotFoundError as ex:
+    if not os.path.isfile('../include/WebContent.h'):
+        raise ex
+    print("WARNING: html header files not found in `html/headers` but `../include/WebContent.h` exists, using old `WebContent.h` instead")
